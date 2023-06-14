b61219100c73e45338b9ec226aa180c1a51e172b
Angel <angel-ortega27@hotmail.com>
2023-06-14 15:46:05 -0700

    added About component and added details, modified Info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Info from "./components/Info";
 import "./App.css";
+import About from "./components/About";
 
 export default function App() {
   return (
     <div className="app--body">
       <Info />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about--container">
+      <h1 className="about--header">About</h1>
+      <main className="about--main">
+        Hello, I'm Angel a Full Stack Developer. I am a self-taught dev that has
+        spent the last two years learning about Web Development and building
+        some cool apps along the way. I am always seeking new challenges and
+        keeping up with the latest trends.
+      </main>
+    </div>
+  );
+}
