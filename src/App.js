441293a685db16bae52b3433e4b447bc55807e6c
Angel <angel-ortega27@hotmail.com>
2023-06-14 15:54:15 -0700

    added Interests component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Info from "./components/Info";
 import "./App.css";
 import About from "./components/About";
+import Interests from "./components/Interests";
 
 export default function App() {
   return (
     <div className="app--body">
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
---
+++ src/components/Interests.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Interests() {
+  return (
+    <div className="interests--container">
+      <h1 className="interests--header">Interests</h1>
+      <main className="interests--main">
+        My interests include working on coding projects primarally React. I
+        enjoy gaming when I get the chance. Currentlty I am playing{" "}
+        <b>
+          <i>Assassins Creed: Origins</i>
+        </b>{" "}
+        and have a habit to earn Platinum trophies in games I play. I am also a
+        dog dad and have two dogs: Halibut and Coqueta who I parent with my
+        amazing wife.
+      </main>
+    </div>
+  );
+}
